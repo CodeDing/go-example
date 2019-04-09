de388a1e986400b18312970307526e4dbdb051aa
dingximing <dingximing@shhuzhong.com>
2019-04-09 17:14:19 +0800

    [FIX]

--- logs/smtp.go
+++ logs/smtp.go
@@ -6,9 +6,19 @@ import (
 	"github.com/astaxie/beego/logs"
 )
 
+var SMTP_CONF = `
+{
+	"username":"tommyhey@163.com",
+	"password":"ding123456",
+	"host":"smtp.163.com:25",
+	"fromAddress":"tommyhey@163.com",
+	"sendTos":["dingximing@shhuzhong.com"]
+}
+`
+
 func main() {
 	log := logs.NewLogger(10000)
-	log.SetLogger("smtp", `{"username":"tommyhey@163.com","password":"xxxxxx","host":"smtp.163.com:25","fromAddress":"tommyhey@163.com","sendTos":["dingximing@shhuzhong.com"]}`)
+	log.SetLogger("smtp", SMTP_CONF)
 	log.Critical("sendmail critical")
 	time.Sleep(time.Second * 30)
 }
